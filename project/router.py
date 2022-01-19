77114c720de8290a78e20941cba4f2817fcf8e76
unknown <mauribarrera23@gmail.com>
2022-01-19 20:58:12 -0300

    Se agregaron rutas para las apis

--- project/router.py
+++ project/router.py
@@ -1,3 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
+from ecommerce.api import ProductViewSet
+
 router = DefaultRouter()
+
+router.register('product', ProductViewSet, basename='product')
