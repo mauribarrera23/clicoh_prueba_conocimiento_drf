d7241f0d21c3f236b2eef9543844b3a136243c42
unknown <mauribarrera23@gmail.com>
2022-01-19 17:25:46 -0300

    Inicialización del proyecto

---
+++ README.md
@@ -0,0 +1,14 @@
+ClicOh | Prueba de conocimiento con Django Rest Framework
+======================
+
+
+### Descripción
+
+
+## Instalación
+
+1. Creación de un entorno virtual.
+2. Instalacion de dependencias: `pip install -r requirements.txt`
+3. Creacion de una base de datos. (Preferentemente PostgreSQL)
+4. Configuracion del proyecto desde el archivo `.env`: `cp env.example .env`
+5. Correr migraciones. `python manage.py migrate`
---
+++ project/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+
+admin.site.site_header = getattr(settings, 'PROJECT_NAME_HEADER')
+admin.site.site_title = getattr(settings, 'PROJECT_NAME_TITLE')
+
+urlpatterns = [
+    path(
+        '',
+        RedirectView.as_view(
+            url=f'{settings.FORCE_SCRIPT_NAME}/admin/' if settings.FORCE_SCRIPT_NAME else '/admin/'
+        )
+    ),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.ACTIVAR_HERRAMIENTAS_DEBUGGING:
+    import debug_toolbar
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
