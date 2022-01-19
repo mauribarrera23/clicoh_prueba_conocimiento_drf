5a3b0a6e437fc6db25c4ff5704ed64483ad79d31
unknown <mauribarrera23@gmail.com>
2022-01-19 20:37:29 -0300

    Se agregaron los modelos Product, Order, Order detail a admin

--- project/apps/ecommerce/admin.py
+++ project/apps/ecommerce/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from ecommerce.models import Product, OrderDetail, Order
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock',)
+    search_fields = ('name',)
+
+
+class OrderDetailStackedInline(admin.StackedInline):
+    model = OrderDetail
+    extra = 0
+    autocomplete_fields = ('product',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_time',)
+    inlines = (OrderDetailStackedInline,)
