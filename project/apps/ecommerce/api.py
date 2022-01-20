304867b6fe98168e55aaf66d550bf9a25d1a1ffb
unknown <mauribarrera23@gmail.com>
2022-01-20 12:31:09 -0300

    Se añadió apis para order detail

--- project/apps/ecommerce/api.py
+++ project/apps/ecommerce/api.py
@@ -1,8 +1,10 @@
+from django.core.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
 
-from ecommerce.models import Product
-from ecommerce.serializers import ProductSerializer
+from ecommerce.models import Product, OrderDetail
+from ecommerce.serializers import ProductSerializer, OrderDetailSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,31 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
     filterset_fields = ['name']
+
+
+class OrderDetailViewSet(viewsets.ModelViewSet):
+    queryset = OrderDetail.objects.all()
+    serializer_class = OrderDetailSerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filterset_fields = ['product__name']
+
+    def perform_create(self, serializer):
+        product = serializer.validated_data['product']
+        order_detail = OrderDetail.objects.filter(order=serializer.validated_data['order'],
+                                                  product=serializer.validated_data['product'].id)
+        if order_detail.exists():
+            raise ValidationError("El producto ya se encuentra seleccionado en la orden.")
+        product.stock = product.stock - serializer.validated_data['quantity']
+        product.save()
+        return serializer.save()
+
+    def perform_update(self, serializer):
+        instance = serializer.instance
+        product = instance.product
+        if instance.quantity < serializer.validated_data['quantity']:
+            product.stock = product.stock - (serializer.validated_data['quantity'] - instance.quantity)
+            product.save()
+        if instance.quantity > serializer.validated_data['quantity']:
+            product.stock = product.stock + (instance.quantity - serializer.validated_data['quantity'])
+            product.save()
+        return serializer.save()
