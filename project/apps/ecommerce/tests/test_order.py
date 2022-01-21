b54f92a599350c1c712e6628cd46f6908f85b691
unknown <mauribarrera23@gmail.com>
2022-01-21 00:31:31 -0300

    Se mejoraron test de order

--- project/apps/ecommerce/tests/test_order.py
+++ project/apps/ecommerce/tests/test_order.py
@@ -1,7 +1,10 @@
 import pytest
 
+from ecommerce.models import Product
 from ecommerce.tests.fixture_user import create_user
 from ecommerce.tests.fixture_order import create_orders
+from ecommerce.tests.fixture_order_detail import create_orders_detail
+from ecommerce.tests.fixture_product import create_products
 from ecommerce.tests.utils import post, delete, get
 
 
@@ -30,8 +33,15 @@ def test_create_order(create_orders):
 
 
 @pytest.mark.django_db
-def test_delete_order(create_orders):
+def test_delete_order(create_orders, create_products, create_orders_detail):
+    product_laptop_lenovo, product_laptop_acer, product_laptop_hp, product_laptop_toshiba, \
+    product_laptop_macbook = create_products
+
     user = create_user(username='superuser')
+
+    product = Product.objects.get(id=product_laptop_acer.id)
+
+    assert product.stock == 7
 
     order_alpha, order_beta, order_gamma = create_orders
 
@@ -41,9 +51,13 @@ def test_delete_order(create_orders):
 
     assert response.status_code == 204
 
+    product.refresh_from_db()
+
+    assert product.stock == 11
+
 
 @pytest.mark.django_db
-def test_order_detail(create_orders):
+def test_order_detail(create_orders, create_orders_detail):
     user = create_user(username='superuser')
 
     order_alpha, order_beta, order_gamma = create_orders
@@ -54,7 +68,10 @@ def test_order_detail(create_orders):
 
     assert response.status_code == 200
 
-    # Agregar fixture de order detail para realizar asserts
+    data = response.json()['data']
+
+    assert data['attributes']['date_time'] == '2022-01-18T18:00:00-03:00'
+    assert data['attributes']['total'] == 2040000.0
 
 
 @pytest.mark.django_db
